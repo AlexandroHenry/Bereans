b231eea93e44fbc0792166bacfa39c8ee252e2a8
alaxhenry <alaxhenry0121@gmail.com>
2022-12-08 18:05:48 +0900

    Initial Commit

---
+++ Bereans/BereansApp.swift
@@ -0,0 +1,17 @@
+//
+//  BereansApp.swift
+//  Bereans
+//
+//  Created by Seungchul Ha on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct BereansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
